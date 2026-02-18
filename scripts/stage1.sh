dca8e0cab864d21de7fa30f68bd0deeb5b8e2d45
lst627 <61502243+lst627@users.noreply.github.com>
2026-02-18 00:08:39 -0800

    update redacted script

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,14 +1,15 @@
 cd cardiac_fm
+export WEIGHTS_PATH=your_model_weights_path
 
 python stage1_CL.py \
   --lr 1e-4 \
   --epochs 20 \
   --batch_size 32 \
-  --mri_csv_path /chru/analysis/lifm6/multi-modal/data_train_valid_test_individual/stage1 \
-  --cropped_mri_path /chru/data/UKBB/MRI/HeartMRI/cropped \
-  --ecg_tsv_path /chru/analysis/lifm6/multi-modal/data_train_valid_test_individual/stage1/ecg_tsv \
-  --save_path /chru/analysis/lifm6/multi-modal/model_state_dict_train_valid_test_individual/stage1_CL1e-4_test \
-  --pt_mri_path /chru/analysis/lifm6/multi-modal/model_state_dict_train_valid_test_individual/cnn_lstm_lvef/model_epoch_70.pt \
-  --pt_ecg_path /chru/analysis/lifm6/mimic_iv_ecg_physionet_pretrained.pt \
+  --mri_csv_path your_mri_csv_path \
+  --cropped_mri_path your_cropped_mri_path \
+  --ecg_tsv_path your_ecg_tsv_path \
+  --save_path your_save_path \
+  --pt_mri_path your_mri_pretrained_path \
+  --pt_ecg_path your_ecg_pretrained_path \
   --wandb \
   --dry_run 
